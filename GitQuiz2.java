df62d33e7b309ae658cbe89b7e64be4e27684e1e
KeiroAkiza <31546511+KeiroAkiza@users.noreply.github.com>
2017-09-08 13:07:30 -0400

    Add files via upload

---
+++ GitQuiz2.java
@@ -0,0 +1,27 @@
+/*
+ * Your job is simple. Create method with your lastname. Make it System.out.println a joke.
+ * Call the method in the main. 
+ * Send it back to my repository.
+ */
+package gitquiz2;
+
+/**
+ *
+ * @author lee_c
+ */
+public class GitQuiz2 {
+
+    public static void Cottrell(){
+        System.out.println("A DBA walks into a bar, approaches two tables and says ");
+        System.out.println("'Hey, mind if I join you?'");
+        System.out.println("\nThere are 10 types of people in the world: those who understand binary, and those who don't.");
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cottrell();
+    }
+    
+}
